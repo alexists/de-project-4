db237049826d49fd9e043c9e1aefcaa58e8f18d0
alexists <i@wb5.ru>
2023-02-12 22:21:08 +0300

    Delete src/dags/init_schema_dag directory

--- src/dags/init_schema_dag/ddl/2022-10-30-160451-dds-srv_wf_settings-create.sql
+++
@@ -1,6 +0,0 @@
-drop table dds.srv_wf_settings;
-CREATE TABLE IF NOT EXISTS dds.srv_wf_settings (
-    id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    workflow_key varchar NOT NULL UNIQUE,
-    workflow_settings JSON NOT NULL
-);
